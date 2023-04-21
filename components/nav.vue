1c2888d0ed60d03e876aefca3990450fe9920423
KeenanLombard <keenanlombardd2@gmail.com>
2023-04-21 13:13:05 +0200

    self-help

--- components/nav.vue
+++ components/nav.vue
@@ -119,7 +119,7 @@
                       active ? 'bg-gray-100' : '',
                       'block px-4 py-2 text-sm text-gray-700',
                     ]"
-                    >Partner Portal</a
+                    >Self-Help Portal</a
                   >
                 </MenuItem>
               </MenuItems>
