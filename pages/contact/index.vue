4eff9332c6811c88f11abd929475a50cbc18f337
KeenanLombard <keenanlombardd2@gmail.com>
2023-04-24 10:31:10 +0200

    nav link and contact image

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,34 +1,32 @@
 <template lang="">
-  <div class="grid lg:grid-cols-3 md:grid-cols-1">
-    <!-- Contact sub route
-    <p>Book Consult - Free Service</p>
-    <p>Book Technical - Texhnical Advice</p>
-    <p>Bookm Pre-Sales - Partner Service</p>
-    <p>Book Demo - Guided Service</p>
-    <p>Self-Service Portal Login</p> -->
-    <div
-      v-for="item in data"
-      class="block max-w-sm bg-white rounded-lg shadow-lg m-5">
-      <div class="prt-6"></div>
-      <div class="p-6">
-        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800">
-          {{ item.title }}
-        </h5>
-        <h5 class="mb-2 text-orange-300">
-          {{ item.text }}
-        </h5>
-        <p class="mb-4 text-base text-sm text-neutral-600">
-          {{ item.sub_text }}
-        </p>
-        <a :href="item.href">
-          <button
-            type="button"
-            class="inline-block rounded bg-orange-500 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-orange-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-            data-te-ripple-init
-            data-te-ripple-color="light">
-            Learn More
-          </button>
-        </a>
+  <div>
+    <img class="contact" src="~/assets/contact.png" alt="" />
+
+    <div class="grid lg:grid-cols-3 md:grid-cols-1">
+      <div
+        v-for="item in data"
+        class="block max-w-sm bg-white rounded-lg shadow-lg m-5">
+        <div class="prt-6"></div>
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800">
+            {{ item.title }}
+          </h5>
+          <h5 class="mb-2 text-orange-300">
+            {{ item.text }}
+          </h5>
+          <p class="mb-4 text-base text-sm text-neutral-600">
+            {{ item.sub_text }}
+          </p>
+          <a :href="item.href">
+            <button
+              type="button"
+              class="inline-block rounded bg-orange-500 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-orange-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+              data-te-ripple-init
+              data-te-ripple-color="light">
+              Learn More
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -82,5 +80,8 @@ const data = ref([
   .wrap {
     flex-direction: column;
   }
+}
+.contact {
+  width: 26rem;
 }
 </style>
